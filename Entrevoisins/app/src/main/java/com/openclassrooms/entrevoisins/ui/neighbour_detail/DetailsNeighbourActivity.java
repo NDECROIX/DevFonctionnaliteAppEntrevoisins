8ca7f52454b8ba057e9a22a45c836da229891d89
NDECROIX <nicolas.decroixx@gmail.com>
2019-06-28 14:23:31 +0200

    Add function newIntent and onOffsetChanged

--- Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_detail/DetailsNeighbourActivity.java
+++ Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_detail/DetailsNeighbourActivity.java
@@ -1,6 +1,9 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_detail;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,7 +23,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class DetailsNeighbourActivity extends AppCompatActivity {
+public class DetailsNeighbourActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
 
     public static final String NEIGHBOUR = "Neighbour";
 
@@ -39,8 +42,14 @@ public class DetailsNeighbourActivity extends AppCompatActivity {
     @BindView(R.id.activity_details_content_tv_facebook)
     TextView mFacebook;
 
+    @BindView(R.id.activity_details_tv_name)
+    TextView mTitleName;
+
     @BindView(R.id.activity_details_collapsing_toolbar)
     CollapsingToolbarLayout collapsingToolbarLayout;
+
+    @BindView(R.id.activity_details_app_bar)
+    AppBarLayout appBarLayout;
 
     private Neighbour mNeighbour;
     private NeighbourApiService mApiService;
@@ -52,16 +61,28 @@ public class DetailsNeighbourActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         mApiService = DI.getNeighbourApiService();
         configView();
-        configToolbar();
+        configAppBarLayout();
+    }
+
+    public static Intent newIntent(Context context, Neighbour favourite){
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(NEIGHBOUR, favourite);
+        Intent intent = new Intent(context, DetailsNeighbourActivity.class);
+        intent.putExtras(bundle);
+        return intent;
     }
 
     /**
-     * configure the toolbar with the neighbor's name
+     * configure the AppBarLayout
      */
-    private void configToolbar() {
+    private void configAppBarLayout() {
+        collapsingToolbarLayout.setTitleEnabled(false);
+        appBarLayout.addOnOffsetChangedListener(this);
+
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
     }
 
@@ -72,11 +93,14 @@ public class DetailsNeighbourActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         mNeighbour = (Neighbour) bundle.getSerializable(NEIGHBOUR);
 
-        collapsingToolbarLayout.setTitle(this.mNeighbour.getName());
+        mTitleName.setText(mNeighbour.getName());
+        collapsingToolbarLayout.setTitle(mNeighbour.getName());
         mName.setText(mNeighbour.getName());
         mFacebook.append(mNeighbour.getName().toLowerCase());
-        if (mApiService.isFavourite(mNeighbour))
+
+        if (mApiService.isFavourite(mNeighbour)){
             fab.setImageResource(R.drawable.ic_star_yellow_24);
+        }
 
         Glide.with(this)
                 .load(mNeighbour.getAvatarUrl())
@@ -92,6 +116,16 @@ public class DetailsNeighbourActivity extends AppCompatActivity {
             showSnackBar(view, getString(R.string.add_favourite));
         } else {
             showSnackBar(view, getString(R.string.already_favourite));
+        }
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()) {
+            // collapsed
+            collapsingToolbarLayout.setTitleEnabled(true);
+        } else {
+            collapsingToolbarLayout.setTitleEnabled(false);
         }
     }
 
